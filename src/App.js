8aa596cdd98b2f2445baaa4020a3eab5870f2149
Renzo Yanzon <renzoyanzon@gmail.com>
2022-09-19 19:45:16 -0300

    Agrego NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <NavBar />
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coderhouse
         </p>
         <a
           className="App-link"
@@ -18,7 +23,12 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+      </div>
+    
+    </>
+
+
+
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import{Container,Navbar,Nav} from "react-bootstrap";
+
+const NavBar = ()=>{
+    return(
+        <Navbar bg="light" variant="light">
+            <Container>
+            <Navbar.Brand href="#home">
+               Hijos del viento  
+            </Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Windsurf</Nav.Link>
+                <Nav.Link href="#pricing">Kitesurf</Nav.Link>
+                <Nav.Link href="#pricing">Standup</Nav.Link>
+                <Nav.Link href="#pricing">Contacto</Nav.Link>
+            </Nav>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar;
