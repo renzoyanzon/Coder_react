5ab7152b16d482e5198c2c788603f5ea6b422d03
Renzo Yanzon <renzoyanzon@gmail.com>
2022-10-26 18:43:11 -0300

    deploy app

--- src/App.js
+++ src/App.js
@@ -11,17 +11,19 @@ import { CartProvider } from './context/CartContext';
 function App() {
 
   return (
-    <CartProvider basename='/Coder_react'>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={ <ItemListContainer greeting={'Navegando juntos'}/>} />
-          <Route path='/category/:categoryName' element={ <ItemListContainer greeting={'Navegando juntos'}/>} /> 
-          <Route path='/item/:id' element={ <ItemDetailContainer/>} />
-          <Route path='/cart' element={ <Cart/>} />         
-        </Routes>  
+    
+      <BrowserRouter basename='/Coder_react'>
+          <CartProvider>
+            <NavBar />
+            <Routes>
+              <Route path='/' element={ <ItemListContainer greeting={'Navegando juntos'}/>} />
+              <Route path='/category/:categoryName' element={ <ItemListContainer greeting={'Navegando juntos'}/>} /> 
+              <Route path='/item/:id' element={ <ItemDetailContainer/>} />
+              <Route path='/cart' element={ <Cart/>} />         
+            </Routes>  
+          </CartProvider>
       </BrowserRouter>
-    </CartProvider>
+
   );
 }
 
